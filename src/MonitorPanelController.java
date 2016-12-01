a6017ca780b2c7c59c29d57ab5317e2905bf32dd
Jason Ho <13210130@life.hkbu.edu.hk>
2016-12-02 02:25:49 +0800

    Fixed Monitor Bug

--- src/MonitorPanelController.java
+++ src/MonitorPanelController.java
@@ -33,6 +33,7 @@ public class MonitorPanelController {
             });
         }
         UpdateQueueStatus();
+        queueStatus.setStyle("-fx-font: 20 arial;");
         queueStatus.setEditable(false);
     }
 
